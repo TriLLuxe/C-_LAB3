5ff7780d8489b74cd3e21cf6cdb3ce0ed5e5687f
TriLLuxe <glazunovkd@gmail.com>
2024-11-01 15:36:48 +0700

    Влад закинь код в бота

--- LAB3/Program.cs
+++ LAB3/Program.cs
@@ -19,9 +19,9 @@ public abstract class Expr : IExpr
     public abstract bool IsConstant { get; }
     public abstract bool IsPolynomial { get; }
     public abstract int PolynomialDegree { get; }
-    public abstract double Compute(IReadOnlyDictionary<string, double> variableValues = null);
+    public abstract double Compute(IReadOnlyDictionary<string, double> variableValues);
 
-    public static implicit operator Expr(int arg) => new Constant(arg);//тут надо Expr сделать от double
+    public static implicit operator Expr(double arg) => new Constant(arg);
     public static Expr Sqrt(Expr operand) => new Sqrt(operand);
 
 
@@ -56,12 +56,20 @@ public class Variable : Expr
 
     public Variable(string name) => Name = name;
 
-    public override IEnumerable<string> Variables => new[] { Name };//так не надо, лучше через yield return, либо один раз надо сохранить, тк тут при изменениии созданется копия
+   public override IEnumerable<string> Variables
+    {
+        get { yield return Name; }
+    }
     public override bool IsConstant => false;
     public override bool IsPolynomial => true;
     public override int PolynomialDegree => 1;
-    public override double Compute(IReadOnlyDictionary<string, double> variableValues) =>
-        variableValues.ContainsKey(Name) ? variableValues[Name] : throw new ArgumentException($"Variable {Name} is not defined.");//лучше try get dictinory тк тут два раза поиск, можно за 1 действие, можно взять просто второе через try catch...
+    public override double Compute(IReadOnlyDictionary<string, double> variableValues)
+    {
+        if (variableValues.TryGetValue(Name, out double value))
+             return value;
+        throw new ArgumentException($"Variable {Name} is not defined.");
+    }
+
     public override string ToString() => Name;
 }
 
@@ -73,15 +81,15 @@ public abstract class UnaryOperation : Expr
 
     public override IEnumerable<string> Variables => Operand.Variables;
     public override bool IsConstant => Operand.IsConstant;
-    public override bool IsPolynomial => false;//тут если знак поменять он останестя полиномом
-    public override int PolynomialDegree => 0;//соответсвтенно тут тоже
+    public override bool IsPolynomial => Operand.IsPolynomial;
+    public override int PolynomialDegree => Operand.PolynomialDegree;
 }
 
 public class UnaryPlus : UnaryOperation
 {
     public UnaryPlus(Expr operand) : base(operand) { }
 
-    public override double Compute(IReadOnlyDictionary<string, double> variableValues = null) => Operand.Compute(variableValues);
+    public override double Compute(IReadOnlyDictionary<string, double> variableValues) => Operand.Compute(variableValues);
     public override string ToString() => $"+({Operand})";
 }
 
@@ -89,7 +97,7 @@ public class UnaryMinus : UnaryOperation
 {
     public UnaryMinus(Expr operand) : base(operand) { }
 
-    public override double Compute(IReadOnlyDictionary<string, double> variableValues = null) => -Operand.Compute(variableValues);
+    public override double Compute(IReadOnlyDictionary<string, double> variableValues) => -Operand.Compute(variableValues);
     public override string ToString() => $"-({Operand})";
 }
 
@@ -105,9 +113,10 @@ public abstract class BinaryOperation : Expr
     }
 
     public override IEnumerable<string> Variables => A.Variables.Union(B.Variables);
+    
     public override bool IsConstant => A.IsConstant && B.IsConstant;//тут неверно (тут больше случаев)
-    public override bool IsPolynomial => A.IsPolynomial && B.IsPolynomial;//тут неверно (хотя хз)
-    public override int PolynomialDegree => Math.Max(A.PolynomialDegree, B.PolynomialDegree);//тут неверно (хотя хз)
+    public override bool IsPolynomial => A.IsPolynomial && B.IsPolynomial;//тут неверно 
+    public override int PolynomialDegree => Math.Max(A.PolynomialDegree, B.PolynomialDegree);//тут неверно 
 }
 
 public class BinaryAddition : BinaryOperation
@@ -118,7 +127,7 @@ public class BinaryAddition : BinaryOperation
     public override string ToString() => $"({A} + {B})";
 }
 
-public class BinarySubtraction : BinaryOperation
+public class BinarySubtraction : BinaryOperation //например тест x^2-x^2 выведет что степень полинома 2. а должна 0
 {
     public BinarySubtraction(Expr a, Expr b) : base(a, b) { }
 
@@ -150,33 +159,33 @@ public class Division : BinaryOperation// в делении не переопр�
     public override string ToString() => $"({A} / {B})";
 }
 
-public class Sqrt : UnaryOperation// тут ближе Function
+public class Sqrt : Function
 {
     public Sqrt(Expr operand) : base(operand) { }
 
-    public override IEnumerable<string> Variables => Operand.Variables;
-    public override bool IsConstant => Operand.IsConstant;
-    public override bool IsPolynomial => false;
-    public override int PolynomialDegree => 0;
+    public override IEnumerable<string> Variables => Val.Variables;
+    public override bool IsConstant => Val.IsConstant;
+    public override bool IsPolynomial => true;
+    public override int PolynomialDegree => Val.PolynomialDegree/2;
 
     public override double Compute(IReadOnlyDictionary<string, double> variableValues)
     {
-        double operandValue = Operand.Compute(variableValues);
+        double operandValue = Val.Compute(variableValues);
         if (operandValue < 0)
             throw new ArgumentException("Square root of a negative number is not allowed.");
         return Math.Sqrt(operandValue);
     }
 
-    public override string ToString() => $"Sqrt({Operand})";
+    public override string ToString() => $"Sqrt({Val})";
 }
 public abstract class Function:Expr
 {
     public Expr Val{ get; }
     public Function(Expr val) => Val = val;
     public override IEnumerable<string> Variables => Val.Variables;
-    public override bool IsConstant => Val.IsConstant;//тут надо переделать
-    public override bool IsPolynomial => Val.IsPolynomial;//тут тоже
-    public override int PolynomialDegree => Val.PolynomialDegree;//и тут посмотреть 
+    public override bool IsConstant => true;
+    public override bool IsPolynomial => true;
+    public override int PolynomialDegree => 0; 
     public static Expr Sin(Expr operand) => new Sin(operand);
     public static Expr Cos(Expr operand) => new Cos(operand);
     public static Expr Tan(Expr operand) => new Tan(operand);
@@ -229,31 +238,16 @@ class Program{
         var x = new Variable("x");
         var y = new Variable("y");
         var c = new Constant(3);
-        var expr1 = (x - 4) * (3*x + y*y) / 5;
-        var expr2 = (5 - 3*c) * Sqrt(16 + c*c);
+        var expr3 = Sin(x);
+        var expr2 =(5 - 3*c) * Sqrt(16 + c*c);
        Console.WriteLine($"""
-        {expr1.ToString()}
-        [{string.Join(", ", expr1.Variables)}]
-        {expr1.IsConstant}
-        {expr1.IsPolynomial}
-        {expr1.PolynomialDegree}
-        {expr1.Compute(new Dictionary<string, double> { { "x", 1 }, { "y", 2 } })}
+        {expr3.ToString()}
+        [{string.Join(", ", expr3.Variables)}]
+        {expr3.IsConstant}
+        {expr3.IsPolynomial}
+        {expr3.PolynomialDegree}
+        {expr3.Compute(new Dictionary<string, double> { { "x", 1 }, { "y", 2 } })}
         """);
-       Console.WriteLine($"""
-        {expr2.ToString()}
-        [{string.Join(", ", expr2.Variables)}]
-        {expr2.IsConstant}
-        {expr2.IsPolynomial}
-        {expr2.PolynomialDegree}
-        {expr2.Compute(new Dictionary<string, double> { { "x", 1 }, { "y", 2 } })}
-        """);
-        var z = new Variable("z");
-        var expr3 =  Cos(z);
-        Console.WriteLine($"{expr3.ToString()}");  
-
-        Console.WriteLine($" {expr3.Compute(new Dictionary<string, double> { { "z", 0.523599 }})}");
-
-        
     }
 
 }
--- LAB3/obj/Debug/net8.0/LAB3.AssemblyInfo.cs
+++ LAB3/obj/Debug/net8.0/LAB3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAB3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e01b7a10922d154bbad07934a92656e07d8bd70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26dca6eba155cdb061f43187510e9df8597e5907")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAB3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAB3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
